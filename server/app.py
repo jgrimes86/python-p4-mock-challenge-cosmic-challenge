b6644cb423429f5872d701462b1459d888f865ee
Nicole Casteel <thepronicole888@gmail.com>
2023-11-17 10:04:39 -0600

    get and get by id complete

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from models import db, Scientist, Mission, Planet
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
+from flask_restful import Api, Resource
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -19,12 +20,30 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+api = Api(app)
 
 
-@app.route('/')
-def home():
-    return ''
+class Scientists(Resource):
 
+    def get(self):
+        scientists = [scientist.to_dict(rules=('-missions',)) for scientist in Scientist.query.all()]
+        return make_response(scientists, 200)
+
+api.add_resource(Scientists, '/scientists')
+
+class ScientistById(Resource):
+
+    def get(self, id):
+        scientist = Scientist.query.filter_by(id = id).first()
+        if scientist: 
+            return make_response(scientist.to_dict(), 200)
+        else:
+            return make_response(
+               {"error": "Scientist not found"},
+               404
+            )
+    
+api.add_resource(ScientistById, '/scientists/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
